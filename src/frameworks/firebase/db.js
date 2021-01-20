08693807e4ec248d47a94a66f881826ed3ea09a3
nizarhalloun <nshalloun@hotmail.com>
2021-01-20 17:52:36 +0200

    db

--- src/frameworks/firebase/db.js
+++ src/frameworks/firebase/db.js
@@ -1,8 +1,28 @@
 import app from './firebase';
 
 const db = app.firestore();
+
 db.settings({
   timestampsInSnapshots: true,
 });
 
-export default db;
+const addAppoinment = ({
+  appointmentType,
+  hospital,
+  year,
+  month,
+  day,
+  hour,
+  mashbetset,
+}) =>
+  db.collection('appointments').add({
+    appointmentType,
+    hospital,
+    year,
+    month,
+    day,
+    hour,
+    mashbetset,
+  });
+
+export { addAppoinment };
