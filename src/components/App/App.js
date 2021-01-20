5bcd7c8f13dd88e531c4418b02f373793ef1628e
rakad-kh <rakad.khawaled@gmail.com>
2021-01-20 20:39:00 +0200

    nav bar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,8 +3,11 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import { Dashboard } from '../pages';
 
+import NavBar from '../NavBar';
+
 const App = () => (
   <Router>
+    <NavBar />
     <Switch>
       <Route exact path="/" component={Dashboard} />
     </Switch>
