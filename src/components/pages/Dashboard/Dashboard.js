3a3bf5195724b35fe1665a9d5d8da60563400f83
nizarhalloun <nshalloun@hotmail.com>
2021-01-20 22:41:04 +0200

    container

--- src/components/pages/Dashboard/Dashboard.js
+++ src/components/pages/Dashboard/Dashboard.js
@@ -1,3 +1,9 @@
-const Dashboard = () => <div>HEY</div>;
+import AppointmentsContainer from '../../AppointmentsContainer';
+import { Fragment } from 'react';
+const Dashboard = () => (
+  <Fragment>
+    <AppointmentsContainer />
+  </Fragment>
+);
 
 export default Dashboard;
