b6fbc4429ca3b42b563ae8de1cc16dfb68f4fb7a
nizarhalloun <nshalloun@hotmail.com>
2021-01-20 22:52:41 +0200

    m

---
+++ src/components/buttons/ConfirmButton/ConfirmButton.js
@@ -0,0 +1,7 @@
+import './ConfirmButton.css';
+
+const ConfirmButton = ({ text }) => {
+  return <button className="confirmButton">{text}</button>;
+};
+
+export default ConfirmButton;
---
+++ src/components/buttons/index.js
@@ -0,0 +1,4 @@
+import NavBarButton from './NavBarButton';
+import ConfirmButton from './ConfirmButton';
+
+export { NavBarButton, ConfirmButton };
